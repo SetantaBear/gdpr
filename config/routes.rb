a656510383714ee21f77432a18b9bcd2aae77034
Fionnuala Hendrick <fionnuala.hendrick@gmail.com>
2017-08-18 10:04:22 +0100

    Finish User Signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,9 @@
 Rails.application.routes.draw do
-
-
-
 root :to => 'static_pages#home'
-
 get '/help' => 'static_pages#help'
-
 get '/about' => 'static_pages#about'
-
 get '/contact' => 'static_pages#contact'
 get '/signup', to: 'users#new'
+post '/signup', to: 'users#create'
+resources :users
 end
